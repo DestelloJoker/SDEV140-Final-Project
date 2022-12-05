024579ad705e1e00682aacfbfdbeac2ebe9ca24e
DestelloJoker <118086926+DestelloJoker@users.noreply.github.com>
2022-12-04 23:07:42 -0500

    Add files via upload
    This is the beginning of my project, it allows you to open an external program with the use of a GUI in python, and the final code will allow you to run the code with a preset of programs you would like to open up when you start your pc up.

---
+++ Final-project-quicksetload.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter import filedialog
+import os
+
+root = Tk()
+root.title('QuickSetLoad')
+root.iconbitmap('Sbstnblnd-Plateau-Apps-amarok.ico')
+root.geometry("600x600")
+
+def open_program():
+    my_program = filedialog.askopenfilename()
+    my_label.config(text=my_program)
+    # Opens the program chosen by the user
+    os.system('"%s"' % my_program)
+
+
+my_button = Button(root, text="Open your program", command=open_program)
+my_button.pack(pady = 20)
+
+my_label = Label(root, text="")
+my_label.pack(pady =20)
+
+root.mainloop()
